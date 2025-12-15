54c81615097e6a2229789e971b518a42a5a875df
Mateusz Papuga <mateuszpapuga24@gmail.com>
2025-12-14 23:58:37 -0800

    💄 make title bigger

--- app/WheelTittle.tsx
+++ app/WheelTittle.tsx
@@ -1,6 +1,6 @@
 export default function WheelTitle() {
   return (
-    <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
+    <h1 className="scroll-m-20 text-center text-5xl font-extrabold tracking-tight text-balance">
       Secrets from the Dungeon Master’s Inventory
     </h1>
   );
