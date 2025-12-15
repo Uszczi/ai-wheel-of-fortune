6dc4f81811d6c3a00382c3b01e7ad1b545b84929
Mateusz Papuga <mateuszpapuga24@gmail.com>
2025-12-15 12:04:02 -0800

    ✨ show the popup on win

--- app/WheelWinner.tsx
+++ app/WheelWinner.tsx
@@ -1,20 +1,26 @@
-// import { useMemo } from "react";
-// import ScrollBanner from "./WinnerPopupScrollBanner";
-// import VictoryShield from "./WinnerPopupVictoryShield";
-// import CrystalTablet from "./WinnerPopupCrystalTablet";
+import { useState, useEffect } from "react";
+import ScrollBanner from "./WinnerPopupScrollBanner";
+import VictoryShield from "./WinnerPopupVictoryShield";
+import CrystalTablet from "./WinnerPopupCrystalTablet";
 
 export default function WheelWinner({ text }: { text: string }) {
-  return <p>{text}</p>;
-  // if (!text) return null;
-  //
-  // const PopupComponent = useMemo(() => {
-  //   const popups = [ScrollBanner, VictoryShield, CrystalTablet];
-  //   return popups[Math.floor(Math.random() * popups.length)];
-  // }, [text]);
-  //
-  // return (
-  //   <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-  //     <PopupComponent text={text} />;
-  //   </div>
-  // );
+  const [Popup, setPopup] = useState<React.ComponentType<{
+    text: string;
+  }> | null>(null);
+
+  useEffect(() => {
+    (() => {
+      const popups = [ScrollBanner, VictoryShield, CrystalTablet];
+      const randomPopup = popups[Math.floor(Math.random() * popups.length)];
+      setPopup(() => randomPopup);
+    })();
+  }, []);
+
+  if (!text || !Popup) return null;
+
+  return (
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <Popup text={text} />
+    </div>
+  );
 }
