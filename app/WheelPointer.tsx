a1c2c0aa755d4e6f1b9d5e034841aa12e49bae72
Mateusz Papuga <mateuszpapuga24@gmail.com>
2025-12-15 20:24:39 -0800

    💄 change mt

--- app/WheelPointer.tsx
+++ app/WheelPointer.tsx
@@ -2,7 +2,7 @@ export default function WheelPointer() {
   return (
     <svg
       viewBox="0 0 40 40"
-      className="mt-6 h-6 w-6 fill-white sm:mt-8 sm:h-8 sm:w-8 md:mt-10 md:h-10 md:w-10"
+      className="mt-4 h-6 w-6 fill-white sm:mt-8 sm:h-8 sm:w-8 md:mt-4 md:h-10 md:w-10"
     >
       <path d="M 20 40 L 0 0 L 40 0 Z" />
     </svg>
